61c3862f60bf50e04ca871115943f334fa16bfe2
Maksim Ostashkov <max23.ost@gmail.com>
2024-12-06 07:08:51 +0300

    minor fix of __init__

--- src/loss/__init__.py
+++ src/loss/__init__.py
@@ -1,3 +1,2 @@
-from src.loss.ctc_loss import CTCLossWrapper
 from src.loss.discriminator_loss import DiscriminatorLoss
 from src.loss.generator_loss import GeneratorLoss
--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,4 +1,3 @@
-from src.model.baseline_model import BaselineModel
 from src.model.hifigan import HiFiGanDiscriminator, HiFiGanGenerator
 
 __all__ = ["BaselineModel", "HiFiGanGenerator", "HiFiGanDiscriminator"]
